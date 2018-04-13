dbce5433f1b6d0221c59a69045537c12c7fd5087
jasonorbaum <jay@notgames.co.uk>
2018-04-13 12:15:01 +0100

    Tidying up

---
+++ NotForBroadcastPOC/Assets/Scripts/BackWallClock.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BackWallClock : MonoBehaviour {
+
+    public Text myDisplay;
+    private float minutes=0;
+    private string minutesString;
+    private float seconds=0;
+    private string secondsString;
+    private float frames=0;
+    private string framesString;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        minutes = Mathf.Floor (Time.time / 60);
+        minutesString = (minutes.ToString());
+        if (minutes<10) { minutesString = "0" + minutesString; }
+
+        seconds = Mathf.Floor (Time.time) % 60;
+        secondsString = (seconds.ToString());
+        if (seconds < 10) { secondsString = "0" + secondsString; }
+
+        frames = Mathf.Floor ((Time.time - Mathf.Floor(Time.time))*30);
+        framesString = (frames.ToString());
+        if (frames < 10) { framesString = "0" + framesString; }
+
+        myDisplay.text = (minutesString+":"+secondsString+":"+framesString);
+	}
+}
